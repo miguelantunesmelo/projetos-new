ddff30f40119b94721915264dfd65c7cf1bd5d88
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-06-18 16:35:00 -0400

    updates

--- calculator/script.js
+++ calculator/script.js
@@ -48,4 +48,28 @@ function handleMath(symbol){
     if(buffer == '0'){
         return;
     }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
 }
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+fun
